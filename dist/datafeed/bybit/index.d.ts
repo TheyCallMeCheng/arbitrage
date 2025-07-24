6179d6493331eef2b40ea4b36581dcf9df829333
TheyCallMeCheng <cheng_corp@protonmail.com>
2025-07-24 16:29:15 +0200

    Stop tracking dist folder - add to .gitignore

--- dist/datafeed/bybit/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { BybitClient, TickerResponse } from "./src/client";
-export { BybitDataFeed, FundingRateData, MultiFundingRateResult } from "./src/index";
-export { BybitClient as default } from "./src/client";
-//# sourceMappingURL=index.d.ts.map
